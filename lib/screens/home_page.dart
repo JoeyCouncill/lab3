f8af1cfb5bdd1adfb37e3f4160e97de6362d01da
joey <joeycouncill@gmail.com>
2023-04-20 13:42:17 -0500

    new commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String userName = "jo co";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("App title on App Bar"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Text(
+         "Hello $userName \n Happy Day",
+          style: TextStyle(color: Colors.white, fontSize: 36.0),
+        ),
+      ),
+    );
+  }
+}
